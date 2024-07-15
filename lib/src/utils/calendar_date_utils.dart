2b6f793aadda867095384cfdae92fd4b02e48f9c
DavidKlz <dev@david-klotz.de>
2024-07-15 13:03:01 +0200

    Update
    * MonthView progress

--- lib/src/utils/calendar_date_utils.dart
+++ lib/src/utils/calendar_date_utils.dart
@@ -1,35 +1,32 @@
 class CalendarDateUtils {
   CalendarDateUtils._();
 
-  static List<DateTime> getDateListFor(int month) {
-    switch (month) {
-      case DateTime.january:
-      // TODO: Implement case
-      case DateTime.february:
-      // TODO: Implement case
-      case DateTime.march:
-      // TODO: Implement case
-      case DateTime.april:
-      // TODO: Implement case
-      case DateTime.may:
-      // TODO: Implement case
-      case DateTime.june:
-      // TODO: Implement case
-      case DateTime.july:
-      // TODO: Implement case
-      case DateTime.august:
-      // TODO: Implement case
-      case DateTime.september:
-      // TODO: Implement case
-      case DateTime.october:
-      // TODO: Implement case
-      case DateTime.november:
-      // TODO: Implement case
-      case DateTime.december:
-        // TODO: Implement case
-        return [];
-      default:
-        throw Exception("invalid month int provided");
+  static List<DateTime> getMonthDateTimesFor(DateTime dateTime) {
+    List<DateTime> monthDateTimes = List.empty(growable: true);
+    var firstDate = _findFirstDateOfTheWeek(_findFirstDateOfTheMonth(dateTime));
+    var lastDate = _findLastDateOfTheWeek(_findLastDateOfTheMonth(dateTime));
+    var currentDate = firstDate;
+    while(currentDate.isBefore(lastDate) || currentDate.isAtSameMomentAs(lastDate)) {
+      monthDateTimes.add(currentDate);
+      currentDate = currentDate.add(const Duration(days: 1));
     }
+    return monthDateTimes;
+  }
+
+  static DateTime _findFirstDateOfTheWeek(DateTime dateTime) {
+    return dateTime.subtract(Duration(days: dateTime.weekday - 1));
+  }
+
+  static DateTime _findLastDateOfTheWeek(DateTime dateTime) {
+    return dateTime
+        .add(Duration(days: DateTime.daysPerWeek - dateTime.weekday));
+  }
+
+  static DateTime _findFirstDateOfTheMonth(DateTime dateTime) {
+    return DateTime(dateTime.year, dateTime.month, 1);
+  }
+
+  static DateTime _findLastDateOfTheMonth(DateTime dateTime) {
+    return DateTime(dateTime.year, dateTime.month + 1, 0);
   }
 }
--- lib/src/views/calendar_month_view.dart
+++ lib/src/views/calendar_month_view.dart
@@ -1,14 +1,89 @@
 import 'package:flutter/material.dart';
 
-class CalendarMonthView extends StatelessWidget {
-  const CalendarMonthView({super.key});
+import '../utils/calendar_date_utils.dart';
+
+class CalendarMonthView extends StatefulWidget {
+  const CalendarMonthView({
+    this.showCalendarWeek = false,
+    this.padding = const EdgeInsets.all(8),
+    this.dayMarkColor = Colors.blueAccent,
+    super.key,
+  });
+
+  final bool showCalendarWeek;
+  final EdgeInsets padding;
+  final Color dayMarkColor;
 
   @override
+  State<CalendarMonthView> createState() => _CalendarMonthViewState();
+}
+
+class _CalendarMonthViewState extends State<CalendarMonthView> {
+  @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: DateTime.daysPerWeek,
-      semanticChildCount: 35,
-      scrollDirection: Axis.horizontal,
+    return LayoutBuilder(
+      builder: (_, constraints) => Table(
+        border: TableBorder.all(color: Colors.grey, width: 0.5),
+        children: _createRows(constraints),
+      ),
     );
+  }
+
+  List<TableRow> _createRows(BoxConstraints constraints) {
+    List<DateTime> month =
+        CalendarDateUtils.getMonthDateTimesFor(DateTime.now());
+    final height = constraints.maxHeight / (month.length / 7);
+    List<TableRow> rows = [];
+    while (month.isNotEmpty) {
+      Iterable<DateTime> take = month.take(7);
+
+      rows.add(
+        TableRow(
+          children: take
+              .map(
+                (e) => TableCell(
+                  child: SizedBox(
+                    height: height,
+                    child: Padding(
+                      padding: widget.padding,
+                      child: Column(
+                        children: [
+                          _createDayText(e),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+      );
+
+      month.removeRange(0, 7);
+    }
+    return rows;
+  }
+
+  Widget _createDayText(DateTime date) {
+    var today = DateTime.now();
+    if (date.day == today.day && date.month == today.month && date.year == today.year) {
+      return SizedBox(
+        width: 28,
+        height: 28,
+        child: ClipOval(
+          child: Container(
+            color: widget.dayMarkColor,
+            alignment: Alignment.center,
+            child: Text(
+              "${date.day}",
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+          ),
+        ),
+      );
+    } else {
+      return Text("${date.day}");
+    }
   }
 }
